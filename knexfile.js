8dfaee4e1b78a59834cb7b21685a7e2d86265a0b
yeeun <yeune90@hanmail.net>
2022-11-16 22:18:24 -0500

    commit

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://goqutnzm:ipHCZCAuJyUgKVGAoqMpl7rnSAnnks4Y@peanut.db.elephantsql.com/goqutnzm",
 } = process.env;
 
 module.exports = {
